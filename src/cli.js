2392e0cb31cf116ca1c5dc3878f49ac8ca3676d2
duker22 <dukin.misha22@gmail.com>
2023-10-14 02:32:15 +0300

    update func in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    console.log("Welcome to the Brain Games!");
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
 
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
+export default greeting;

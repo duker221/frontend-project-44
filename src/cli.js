1e728133fe76fc1b8aec36620400e162192fee7f
duker22 <dukin.misha22@gmail.com>
2023-10-14 01:50:23 +0300

    add greeting func

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = () => {
+    console.log("Welcome to the Brain Games!");
+
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+

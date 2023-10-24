2eb6e328a42a706764674eed71da7f22fa1ff1c0
duker22 <dukin.misha22@gmail.com>
2023-10-25 01:13:23 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const mainEngine = (question, createGameData) => {
   console.log('Welcome to the Brain-Games!');
 
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}`);
+  console.log(`Hello, ${userName}`);
 
   console.log(question);
 

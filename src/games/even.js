82cf616205f2a7e52d6a232342a86b7ab2a750e2
duker22 <dukin.misha22@gmail.com>
2023-10-25 00:35:39 +0300

    update brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -2,13 +2,10 @@ import mainEngine from '../index.js';
 
 const numIsEven = (num) => num % 2 === 0;
 
-const getRandomNumber = (min, max) => {
-  const random = Math.floor(min + Math.random() * Math.floor(max));
-  return random;
-};
+const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const createGameData = () => {
-  const question = getRandomNumber(1, 50);
+  const question = getRandomInt(200);
 
   const answer = numIsEven(question) ? 'yes' : 'no';
   const gameData = [question, answer];

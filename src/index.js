7139d52048428abb9fabf78f6adc67a2f4a07cc2
duker22 <dukin.misha22@gmail.com>
2023-10-28 18:27:33 +0300

    update mainEngine.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 import readlineSync from 'readline-sync';
 
-const roundCount = 3;
-
-const mainEngine = (question, createGameData) => {
+const mainEngine = (discriptions, createGameData) => {
   console.log('Welcome to the Brain-Games!');
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
 
-  console.log(question);
+  console.log(discriptions);
 
-  const roundStart = (count) => {
-    if (count === roundCount) {
-      console.log(`Congratulations, ${userName}!`);
-      return true;
-    }
+  const roundCount = 3;
 
-    const roundData = createGameData();
-    console.log(`Question: ${roundData[0]}`);
+  for (let i = 0; i < roundCount; i++) {              //Переделал на цикл
+    const [question, answer] = createGameData();      //Сразу деструктуризировал массив
+
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    const result = roundData[1] === userAnswer;
-    if (!result) {
-      console.log(`"${userAnswer}" is wrong answer :(. Correct answer is "${roundData[1]}".`);
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer :(. Correct answer is "${answer}".`);
       console.log(`Let's try again, ${userName}!`);
       return false;
-    }
-    console.log('Correct!');
-    return roundStart(count + 1);
+    };
   };
-
-  return roundStart(0);
+  console.log(`Congratulations, ${userName}!`);
+  return;
 };
 
 export default mainEngine;

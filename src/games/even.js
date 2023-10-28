30325e8ab34bc1f6874f911d588190d456971861
duker22 <dukin.misha22@gmail.com>
2023-10-28 18:25:22 +0300

    update even.js. add new func.

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,11 @@
 import mainEngine from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const numIsEven = (num) => num % 2 === 0;
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const createGameData = () => {
-  const question = getRandomInt(200);
+  const question = getRandomNumber(0, 200);
 
   const answer = numIsEven(question) ? 'yes' : 'no';
   const gameData = [question, answer];
@@ -16,3 +16,4 @@ const discriptions = 'Answer "yes" if the number is even, otherwise answer "no".
 
 const startGame = () => mainEngine(discriptions, createGameData);
 export default startGame;
+

c1b1049346e57048e2aaa26ce984afe353fb0162
duker22 <dukin.misha22@gmail.com>
2023-10-31 01:01:08 +0300

    update "commonDivisior" func

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,27 +4,32 @@ import getRandomNumber from '../utils.js';
 const getMaxOfArray = (numArray) => Math.max.apply(null, numArray);
 
 const commonDivisor = (numberA, numberB) => {
+  numberA = Math.abs(numberA);
+  numberB = Math.abs(numberB);
+
   if (numberA === 0 || numberB === 0) {
     return [1];
   }
 
-  const arr = [];
+  const cycleLength = Math.max(numberA, numberB);
 
-  for (let i = 1; i < numberA; i += 1) {
+  const arr = [];
+  
+  for (let i = 1; i <= cycleLength; i += 1) {
     if (numberA % i === 0 && numberB % i === 0) {
       arr.push(i);
     }
   }
-  return arr;
+  return getMaxOfArray(arr);
 };
 
 const createGameData = () => {
   const numberA = getRandomNumber(0, 100);
   const numberB = getRandomNumber(0, 100);
 
-  const question = `${numberA} ${numberB}`;
+  const question = `${numberA}, ${numberB}`;
 
-  const answer = String(getMaxOfArray(commonDivisor(numberA, numberB)));
+  const answer = String((commonDivisor(numberA, numberB)));
 
   const gameData = [question, String(answer)];
   return gameData;
